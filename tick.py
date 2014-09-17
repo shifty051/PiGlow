5028380680439aef3b245de7e55e82403c1b851a
shifty051 <dean.a.cormack@gmail.com>
2014-09-17 18:01:54 +1000

    Update tick.py
    This will make the LEDs turn on in succession up to white, then turn off in reverse order and repeat.
    To change the delay between the LED cycle, set sleep = [desired delay in s]
    To change the brightness of the LEDs, set the value of the colour (piglow.green(#)) to desired brightness (0-255)
      
    eg. piglow.green(1) ~ sets the brightness of the green LEDs to 1.

--- tick.py
+++ tick.py
@@ -11,36 +11,39 @@ piglow = PiGlow()
 while True:
 
   piglow.red(5)
-  sleep(2)
+  sleep(1)
   
   piglow.orange(5)
-  sleep(2)
+  sleep(1)
   
   piglow.yellow(5)
-  sleep(2)
+  sleep(1)
   
   piglow.green(5)
-  sleep(2)
+  sleep(1)
   
   piglow.blue(5)
-  sleep(2)
+  sleep(1)
   
   piglow.white(5)
-  sleep(2)
+  sleep(1)
+  
+  piglow.white(0)
+  sleep(1)
   
   piglow.blue(0)
-  sleep(2)
+  sleep(1)
   
   piglow.green(0)
-  sleep(2)
+  sleep(1)
   
   piglow.yellow(0)
-  sleep(2)
+  sleep(1)
   
   piglow.orange(0)
-  sleep(2)
+  sleep(1)
   
   piglow.red(0)
-  sleep(2)
+  sleep(1)
   
   
