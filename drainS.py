9eec0ecc3cfe12d7b45390ea3422998d91406fdb
shifty051 <dean.a.cormack@gmail.com>
2014-09-17 18:25:21 +1000

    Update drainS.py

--- drainS.py
+++ drainS.py
@@ -102,30 +102,3 @@ while True:
   piglow.white(0)
   sleep(0.1)
   
-  piglow.orange(5)
-  sleep(0.5)
-  
-  piglow.yellow(5)
-  piglow.red(0)
-  sleep(0.5)
-  
-  piglow.green(5)
-  piglow.orange(0)
-  sleep(0.5)
-  
-  piglow.blue(5)
-  piglow.yellow(0)
-  sleep(0.5)
-  
-  piglow.white(5)
-  piglow.green(0)
-  sleep(0.5)
-  
-  piglow.blue(0)
-  sleep(0.5)
-  
-  piglow.white(0)
-  piglow.red(5)
-  sleep(0.5)
-  
-  
