0d92c0f04854ac02e239c1998bac723bde920a31
shifty051 <dean.a.cormack@gmail.com>
2014-09-17 17:56:40 +1000

    Update tick.py

--- tick.py
+++ tick.py
@@ -11,4 +11,36 @@ piglow = PiGlow()
 while True:
 
   piglow.red(5)
-  sleep(5)
+  sleep(2)
+  
+  piglow.orange(5)
+  sleep(2)
+  
+  piglow.yellow(5)
+  sleep(2)
+  
+  piglow.green(5)
+  sleep(2)
+  
+  piglow.blue(5)
+  sleep(2)
+  
+  piglow.white(5)
+  sleep(2)
+  
+  piglow.blue(0)
+  sleep(2)
+  
+  piglow.green(0)
+  sleep(2)
+  
+  piglow.yellow(0)
+  sleep(2)
+  
+  piglow.orange(0)
+  sleep(2)
+  
+  piglow.red(0)
+  sleep(2)
+  
+  
