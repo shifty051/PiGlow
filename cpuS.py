795a37cbda1696ca5214a5ea55b8ee6912c00055
shifty051 <dean.a.cormack@gmail.com>
2014-09-17 17:37:52 +1000

    Update cpuS.py

--- cpuS.py
+++ cpuS.py
@@ -19,28 +19,28 @@ while True:
     piglow.red(10)
 
   elif cpu < 20:
-    piglow.red(10)
-    piglow.orange(10)
+    piglow.red(1)
+    piglow.orange(1)
 
   elif 20 < cpu < 49:
-    piglow.red(10)
-    piglow.orange(10)
-    piglow.yellow(10)
+    piglow.red(1)
+    piglow.orange(1)
+    piglow.yellow(1)
 
   elif cpu < 60:
-    piglow.red(10)
-    piglow.orange(10)
-    piglow.yellow(10)
-    piglow.green(10)
+    piglow.red(1)
+    piglow.orange(1)
+    piglow.yellow(1)
+    piglow.green(1)
 
   elif cpu < 80:
     piglow.red(10)
-    piglow.orange(10)
-    piglow.yellow(10)
-    piglow.green(10)
-    piglow.blue(10)
+    piglow.orange(1)
+    piglow.yellow(1)
+    piglow.green(1)
+    piglow.blue(1)
 
   else:
-    piglow.white(20)
-    sleep(3)
+    piglow.white(1)
+    sleep(1)
 
