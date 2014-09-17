68fe755560aaa4167c94b7295ae09366568f3d92
shifty051 <dean.a.cormack@gmail.com>
2014-09-17 22:27:51 +1000

    Update netsat.py

--- netsat.py
+++ netsat.py
@@ -1,41 +1,45 @@
-import os
-import subprocess
-import time
-from daemon import runner
-import datetime
-import re 
-
-class App():
-    def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
-        self.pidfile_path =  '/tmp/bandwidth_counter.pid'
-        self.pidfile_timeout = 5
-    def run(self):
-        rx_megabits_new = 0
-        tx_megabits_new = 0
-        try:
-                while True:
-                        output = subprocess.Popen(['ifconfig', "wlan0"], stdout=subprocess.PIPE).communicate()[0]
-                        rx_bytes = re.findall('RX bytes:([0-9]*) ', output)[0]
-                        tx_bytes = re.findall('TX bytes:([0-9]*) ', output)[0]
-                        rx_megabits = (((int(rx_bytes) * 8) / 1024) / 1024)
-                        tx_megabits = (((int(tx_bytes) * 8) / 1024) / 1024)
-                        current_rx_usage = rx_megabits - rx_megabits_new
-                        current_tx_usage = tx_megabits - tx_megabits_new
-                        rx_megabits_new = rx_megabits
-                        tx_megabits_new = tx_megabits 
-                        print 'average megabits received', current_rx_usage / 60
-                        print 'average kilobits received', (current_rx_usage * 1024) / 60
-                        print 'average megabits sent', current_tx_usage / 60
-                        print 'average kilobits sent', (current_tx_usage * 1024) / 60
-                        time.sleep(60)
-
-        except Exception, e:
-            raise
-
-
-app = App()
-daemon_runner = runner.DaemonRunner(app)
-daemon_runner.do_action()
+import re
+ 
+def GetInterfaces():
+    ret = {}
+    f = open("/proc/net/dev", "r");
+    data = f.read()
+    f.close()
+ 
+    r = re.compile("[:\s]+")
+ 
+    lines = re.split("[\r\n]+", data)
+    for line in lines[2:]:
+        columns = r.split(line)
+        if len(columns) < 18:
+            continue
+        info                  = {}
+        info["rx_bytes"]      = columns[2]
+        info["rx_packets"]    = columns[3]
+        info["rx_errors"]     = columns[4]
+        info["rx_dropped"]    = columns[5]
+        info["rx_fifo"]       = columns[6]
+        info["rx_frame"]      = columns[7]
+        info["rx_compressed"] = columns[8]
+        info["rx_multicast"]  = columns[9]
+ 
+        info["tx_bytes"]      = columns[10]
+        info["tx_packets"]    = columns[11]
+        info["tx_errors"]     = columns[12]
+        info["tx_dropped"]    = columns[13]
+        info["tx_fifo"]       = columns[14]
+        info["tx_frame"]      = columns[15]
+        info["tx_compressed"] = columns[16]
+        info["tx_multicast"]  = columns[17]
+ 
+        iface                 = columns[1]
+        ret[iface] = info
+    return ret
+ 
+interfaces = GetInterfaces()
+for(iface, info) in interfaces.items():
+ 
+    print "Interface: %s" % (iface)
+    for (metric, value) in info.items():
+        print "% 10s% 20s = %s" % (" " * 10, metric, value)
+    print
