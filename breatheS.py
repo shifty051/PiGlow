c7bc83070a994fe3f32dc3ef7341ef9d913dbfac
shifty051 <dean.a.cormack@gmail.com>
2014-09-17 18:10:53 +1000

    Update breatheS.py

--- breatheS.py
+++ breatheS.py
@@ -1,33 +1,41 @@
-#########################################################
-# Set the Piglow To breathe function ##
+###############################################################
+# Set the LEDs to turn on/off in sequence in a breathing fashion
 # shifty051
-#########################################################
+###############################################################
 
 from piglow import PiGlow
 from time import sleep
 
 piglow = PiGlow()
 
-  piglow.all(0)
+while True:
 
-  while True:
-    piglow.red(5)
-    sleep(3)
-    
-    piglow.orange(5)
-    sleep(3)
-    
-    piglow.yellow(5)
-    sleep(3)
-    
-    piglow.green(5)
-    sleep(3)
-    
-    piglow.blue(5)
-    sleep(3)
-    
-    piglow.white(5)
-    sleep(3)
-    
-    
-    
+  piglow.red(5)
+  sleep(0.5)
+  
+  piglow.orange(5)
+  sleep(0.5)
+  
+  piglow.yellow(5)
+  piglow.red(0)
+  sleep(0.5)
+  
+  piglow.green(5)
+  piglow.orange(0)
+  sleep(0.5)
+  
+  piglow.blue(5)
+  piglow.yellow(0)
+  sleep(0.5)
+  
+  piglow.white(5)
+  piglow.green(0)
+  sleep(0.5)
+  
+  piglow.blue(0)
+  sleep(0.5)
+  
+  piglow.white(0)
+  sleep(0.5)
+  
+  
