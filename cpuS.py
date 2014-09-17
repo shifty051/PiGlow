36179a0bec2ccfebed3a4e2ee085a8b0ec689cf2
shifty051 <dean.a.cormack@gmail.com>
2014-09-17 16:36:59 +1000

    Update cpuS.py

--- cpuS.py
+++ cpuS.py
@@ -9,36 +9,38 @@ from time import sleep
 import psutil
 
 piglow = PiGlow()
+
 while True:
-cpu = psutil.cpu_percent()
-piglow.all(0)
+  
+  cpu = psutil.cpu_percent()
+  piglow.all(0)
 
-if cpu < 5:
-piglow.red(10)
+  if cpu < 5:
+    piglow.red(10)
 
-elif cpu < 20:
-piglow.red(10)
-piglow.orange(10)
+  elif cpu < 20:
+    piglow.red(10)
+    piglow.orange(10)
 
-elif 20 < cpu < 49:
-piglow.red(10)
-piglow.orange(10)
-piglow.yellow(10)
+  elif 20 < cpu < 49:
+    piglow.red(10)
+    piglow.orange(10)
+    piglow.yellow(10)
 
-elif cpu < 60:
-piglow.red(10)
-piglow.orange(10)
-piglow.yellow(10)
-piglow.green(10)
+  elif cpu < 60:
+    piglow.red(10)
+    piglow.orange(10)
+    piglow.yellow(10)
+    piglow.green(10)
 
-elif cpu < 80:
-piglow.red(10)
-piglow.orange(`0)
-piglow.yellow(10)
-piglow.green(10)
-piglow.blue(10)
+  elif cpu < 80:
+    piglow.red(10)
+    piglow.orange(`0)
+    piglow.yellow(10)
+    piglow.green(10)
+    piglow.blue(10)
 
-else:
-piglow.white(20)
-sleep(0.5)
+  else:
+    piglow.white(20)
+    sleep(0.5)
 
