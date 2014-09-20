bfab7f063abe6e77f8768b7d8a77de7380ede03d
shifty051 <dean.a.cormack@gmail.com>
2014-09-20 22:32:30 +1000

    Update brightS.py

--- brightS.py
+++ brightS.py
@@ -120,24 +120,142 @@ while True:
   piglow.led(7,2)
   sleep(0.1)
   
-  piglow.led(1,3)
   piglow.led(7,3)
+  piglow.led(13,1)
+  sleep(0.1)
+  
+  piglow.led(7,4)
+  piglow.led(13,2)
+  sleep(0.1)
+  
+  piglow.led(13,3)
+  piglow.led(2,1)
+  sleep(0.1)
+  
+  piglow.led(13,4)
+  piglow.led(2,2)
+  sleep(0.1)
+  
+  piglow.led(2,3)
+  piglow.led(8,1)
+  sleep(0.1)
+  
+  piglow.led(2,4)
+  piglow.led(8,2)
+  sleep(0.1)
+  
+  piglow.led(8,3)
+  piglow.led(14,1)
+  sleep(0.1)
+  
+  piglow.led(8,4)
+  piglow.led(14,2)
+  sleep(0.1)
+  
+  piglow.led(14,3)
+  piglow.led(3,1)
+  sleep(0.1)
+  
+  piglow.led(14,4)
+  piglow.led(3,2)
+  sleep(0.1)
+  
+  piglow.led(3,3)
+  piglow.led(9,1)
+  sleep(0.1)
+  
+  piglow.led(3,4)
+  piglow.led(9,2)
+  sleep(0.1)
+  
+  piglow.led(9,3)
+  piglow.led(15,1)
+  sleep(0.1)
+  
+  piglow.led(9,4)
+  piglow.led(15,2)
+  sleep(0.1)
+  
+  piglow.led(15,3)
+  piglow.led(4,1)
+  sleep(0.1)
+  
+  piglow.led(15,4)
+  piglow.led(4,2)
+  sleep(0.1)
+  
+  piglow.led(4,3)
+  piglow.led(10,1)
+  sleep(0.1)
+  
+  piglow.led(4,4)
+  piglow.led(10,2)
+  sleep(0.1)
+  
+  piglow.led(10,3)
+  piglow.led(16,1)
+  sleep(0.1)
+  
+  piglow.led(10,4)
+  piglow.led(16,2)
+  sleep(0.1)
+  
+  piglow.led(16,3)
+  piglow.led(5,1)
+  sleep(0.1)
+  
+  piglow.led(16,4)
+  piglow.led(5,2)
+  sleep(0.1)
+  
+  piglow.led(5,3)
+  piglow.led(11,1)
+  sleep(0.1)
+  
+  piglow.led(5,4)
+  piglow.led(11,2)
+  sleep(0.1)
+  
+  piglow.led(11,3)
+  piglow.led(17,1)
+  sleep(0.1)
+  
+  piglow.led(11,4)
+  piglow.led(17,2)
+  sleep(0.1)
+  
+  piglow.led(17,3)
+  piglow.led(6,1)
+  sleep(0.1)
+  
+  piglow.led(17,4)
+  piglow.led(6,2)
+  sleep(0.1)
+  
+  piglow.led(6,3)
   piglow.led(12,1)
   sleep(0.1)
   
-  piglow.led(1,2)
-  piglow.led(7,4)
+  piglow.led(6,4)
   piglow.led(12,2)
   sleep(0.1)
   
-  piglow.led(1,1)
-  piglow.led(7,3)
   piglow.led(12,3)
+  piglow.led(18,1)
   sleep(0.1)
   
-  piglow.led(1,0)
-  piglow.led(7,2)
   piglow.led(12,4)
+  piglow.led(18,2)
   sleep(0.1)
+  
+  piglow.led(18,3)
+  sleep(0.1)
+  
+  piglow.led(18,4)
+  sleep(0.1)
+  
+  
+  
+  
   
   
